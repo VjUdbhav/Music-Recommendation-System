1f2364e4e15b1c3735e236d87cd97128cafa2d22
VjUdbhav <vjudbhav@gmail.com>
2026-01-18 21:51:51 +0530

    Libraries imported in project

---
+++ Music_Recommendation_System.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import matplotlib.pyplot as plt
+import seaborn as sns
