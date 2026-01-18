0e04487a986f4ea0ed636ed9fc494205b8bb9e21
VjUdbhav <vjudbhav@gmail.com>
2026-01-18 22:03:05 +0530

    Pre Data Visualization and Data Preprocessing was done in the program

--- Music_Recommendation_System.py
+++ Music_Recommendation_System.py
@@ -4,3 +4,13 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+data = pd.read_csv('C:\\Users\\Udbhav\\Documents\\Music Recommendation System\\Music-Recommendation-System\\tcc_ceds_music.csv')  
+data.head()
+
+plt.figure(figsize=(10, 6))
+sns.countplot(y='genre', data=data, order=data['genre'].value_counts().index[:10])
+plt.title('Top 10 Genres')
+plt.xlabel('Count')
+plt.ylabel('Genre')
+plt.show()
