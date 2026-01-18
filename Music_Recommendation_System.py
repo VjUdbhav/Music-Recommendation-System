0d5bcbac9154aea5869bf1abdecabe92b30e140a
VjUdbhav <vjudbhav@gmail.com>
2026-01-18 22:07:29 +0530

    Completed the data preprocessing and trained the recommendation system

--- Music_Recommendation_System.py
+++ Music_Recommendation_System.py
@@ -5,12 +5,75 @@ from sklearn.metrics.pairwise import cosine_similarity
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Load the dataset
 data = pd.read_csv('C:\\Users\\Udbhav\\Documents\\Music Recommendation System\\Music-Recommendation-System\\tcc_ceds_music.csv')  
 data.head()
 
+# Top 10 Genres Visualization
 plt.figure(figsize=(10, 6))
 sns.countplot(y='genre', data=data, order=data['genre'].value_counts().index[:10])
 plt.title('Top 10 Genres')
 plt.xlabel('Count')
 plt.ylabel('Genre')
+plt.show()
+
+# Top 10 Artists Visualization
+top_artists = data.groupby('artist_name').size().sort_values(ascending=False).head(10)
+plt.figure(figsize=(10, 6))
+sns.barplot(x=top_artists.values, y=top_artists.index, palette='viridis')
+plt.title('Top 10 Artists by Number of Songs')
+plt.xlabel('Number of Songs')
+plt.ylabel('Artist Name')
+plt.show()
+
+# Data Preprocessing
+
+# Combining features
+data['combined_features'] = (
+    data['genre'].fillna('') + ' ' +
+    data['artist_name'].fillna('') + ' ' +
+    data['track_name'].fillna('')
+)
+
+# Vectorization
+tfidf = TfidfVectorizer(stop_words='english')
+tfidf_matrix = tfidf.fit_transform(data['combined_features'])
+
+#Score Computation
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
+def get_recommendations(song_title, data, cosine_sim, top_n=10):
+
+    idx = data[data['track_name'] == song_title].index
+    if len(idx) == 0:
+        print("Song not found in the dataset.")
+        return
+    
+    idx = idx[0]
+
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    sim_scores = sim_scores[1:top_n+1]  
+    song_indices = [i[0] for i in sim_scores]
+
+    recommendations = data.iloc[song_indices]
+    return recommendations
+
+# Data Preprocessing Complete
+
+
+# Get Recommendations
+
+recommended_songs = get_recommendations('cry', data, cosine_sim, top_n=10)
+print(recommended_songs[['track_name', 'artist_name', 'genre']])
+
+# Visualize Recommendations
+
+plt.figure(figsize=(10, 6))
+sns.barplot(y='track_name', x='artist_name', data=recommended_songs, palette='coolwarm')
+plt.title('Recommended Songs Similar to "Cry"')
+plt.xlabel('Artist Name')
+plt.ylabel('Song Name')
 plt.show()
--- README.md
+++ README.md
@@ -9,3 +9,9 @@ The dataset has been aquired from GeeksForGeeks.org
 
 18/02/2026 | 9:46 PM
 Python program file "Music_Recommendation_System.py" was created.
+
+18/02/2026 | 9:56 PM
+Plotted a bar graph for the top 10 genres and top 10 artists as per the dataset.
+
+18/02/2026 | 10:05 PM
+Completed data preprocessing and visualized the data.
